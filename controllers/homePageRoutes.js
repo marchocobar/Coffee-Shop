1ebb463a288184a87189d327ac73ed7cdc2113cc
kassing3 <108312371+kassing3@users.noreply.github.com>
2022-12-01 20:28:10 -0500

    Updated Sign up

--- controllers/homePageRoutes.js
+++ controllers/homePageRoutes.js
@@ -71,10 +71,10 @@ router.get('/login', (req, res) => {
 
 router.get('/signup', (req, res) => {
   // If the user is already logged in, redirect the request to another route
-  // if (req.session.logged_in) {
-  //   res.redirect("/member");
-  //   return;
-  // }
+  if (req.session.logged_in) {
+    res.redirect("/member");
+    return;
+  }
 
   res.render('signup');
 });
