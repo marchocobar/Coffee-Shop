091ab6d44d736921229279d64e2d74502694cb11
Marissa Baron <mariluvv23@gmail.com>
2022-12-01 19:56:12 -0500

    merge

--- public/js/login.js
+++ public/js/login.js
@@ -16,7 +16,7 @@ const loginFormHandler = async (event) => {
       if (response.ok
         ) {
         // If successful, redirect the browser to the profile page
-        document.location.replace('/api/member');
+        document.location.replace('/memberProfile');
        
        
       } else {
